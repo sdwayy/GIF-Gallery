0bb8b4d7167df9a08b5671e4ae25d203f15370ef
Daniil <set2901@mail.ru>
2021-04-21 15:21:51 +0300

    made refactoring

--- src/core/api.ts
+++ src/core/api.ts
@@ -1,9 +1,9 @@
 const API_KEY = '1fvGjMHdW85x3VmAklanxUniZW7thdEy';
 
 export default async function getGifByTag(gifTag: string) {
-  const getUrl = (tag: string) => `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+  const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${gifTag}`;
 
-  const gifData = await fetch(getUrl(gifTag))
+  const gifData = await fetch(url)
     .then((response) => response.json())
     .catch((error) => {
       throw new Error(`Произошла HTTP ошибка: ${error.message}`);
